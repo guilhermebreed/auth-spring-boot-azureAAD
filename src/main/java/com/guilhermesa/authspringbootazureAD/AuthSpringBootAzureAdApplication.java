537c3b494bddb71d235ed028b33802091974aa3c
Guilherme Silva <guilherme.bralves@hotmail.com>
2018-06-21 11:11:20 -0300

    first

---
+++ src/main/java/com/guilhermesa/authspringbootazureAD/AuthSpringBootAzureAdApplication.java
@@ -0,0 +1,12 @@
+package com.guilhermesa.authspringbootazureAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringBootAzureAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringBootAzureAdApplication.class, args);
+	}
+}
